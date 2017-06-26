d58339023f4e9727c4a9dde517d19bf3bf87a995
Varun Kohli <kohli.varun5@gmail.com>
2017-06-25 20:57:01 -0400

    .

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const SOURCE_FILE_OF_NAME = {
 
 exports.handler = (event, context, callback) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
+
     const done = (err, res) => callback(null, {
         statusCode: err ? '400' : '200',
         body: err ? err.message : JSON.stringify(res),
