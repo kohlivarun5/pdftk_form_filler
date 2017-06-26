9cf9bd4f8b4299991abfc7e97391f516d8d769fc
Varun Kohli <kohli.varun5@gmail.com>
2017-06-25 20:25:43 -0400

    upload to s3

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 'use strict';
 
 const pdfFiller   = require( 'node-pdffiller' );
+const AWS = require('aws-sdk');
+const fs = require('fs');
+
+AWS.config.update({
+  accessKeyId: 'AKIAIK3KOW5ILAEFVSIQ',
+  secretAccessKey: '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' 
+});
+var s3 = new AWS.S3();
 
 const SOURCE_FILE_OF_NAME = {
   "I539" : {
@@ -31,9 +39,18 @@ exports.handler = (event, context, callback) => {
           var body = JSON.parse(event.body);
           console.log(body);
           var sourcePDF = SOURCE_FILE_OF_NAME[body.form_name].path;
+          var id = context.requestId;
+          var destinationPDF = "/tmp/${id}.pdf";
           pdfFiller.fillForm(
-            sourcePDF, "complete.pdf", body.form_data, function(err) { 
-              done(err,"In callback (we're done)."); 
+            sourcePDF, destinationPDF, body.form_data, function(err) { 
+              fs.readFile(destinationPDF, function (err, data) {
+                if (err) { return done(err,"Failed to read!"); }
+              
+                var base64data = new Buffer(data, 'binary');
+              
+                var params = {Bucket: 'pdftkapi-output', Key: id, Body:base64data};
+                s3.upload(params, done);
+              });
           });
           break;
         default:
